e554de6eb93dbb3acb0e84767003b6d0e36052db
TraineePilotLeo <135687748+TraineePilotLeo@users.noreply.github.com>
2023-06-06 15:45:49 +1000

    Update bd-scss.config.js

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -7,7 +7,7 @@ export default {
 		description: 'Display your picture of choice with adjustable blur and brightness. Dark theme is required.',
 		invite: 'ZHthyCw',
 		authorId: '174868361040232448',
-		source: 'https://github.com/DiscordStyles/FrostedGlass',
+		source: 'https://images.ctfassets.net/cnu0m8re1exe/6sLnMf6GKSgsXxbJbpGXWO/7ea7a98ebc24318f0d60b1f5484053de/shutterstock_707735803.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill',
 		website: 'https://gibbu.me/'
 	},
 	base: {
