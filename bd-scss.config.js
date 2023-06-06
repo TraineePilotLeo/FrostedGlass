9983a7ea2630e84378bd32d052d9c1d63a9db3c5
TraineePilotLeo <135687748+TraineePilotLeo@users.noreply.github.com>
2023-06-06 15:46:47 +1000

    Update bd-scss.config.js

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -8,7 +8,7 @@ export default {
 		invite: 'ZHthyCw',
 		authorId: '174868361040232448',
 		source: 'https://images.ctfassets.net/cnu0m8re1exe/6sLnMf6GKSgsXxbJbpGXWO/7ea7a98ebc24318f0d60b1f5484053de/shutterstock_707735803.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill',
-		website: 'https://gibbu.me/'
+		website: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.discovermagazine.com%2Fthe-sciences%2Fwhat-are-the-safest-seats-on-a-plane-and-13-other-airplane-safety-questions&psig=AOvVaw3m2KCHcpPJhzhfKf9ZTJA_&ust=1686116598580000&source=images&cd=vfe&ved=0CBIQjhxqFwoTCKDijvn3rf8CFQAAAAAdAAAAABAE'
 	},
 	base: {
 		output: 'dist/dist'
